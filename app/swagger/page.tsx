707ea9879d4efd678de6577fa008e1def18ef7b3
yusei-iwaki <iwaki.yusei11@gmail.com>
2026-03-20 14:36:15 +0900

    feat: swaggerの作成

---
+++ app/swagger/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+
+export default function Page() {
+    return(
+        <SwaggerUI url="/openapi/openapi.yml"></SwaggerUI>
+    )
+}
